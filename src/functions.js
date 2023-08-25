d568134d30d0fa3344c50f4e87cf898941d2f735
Artem Ufimtcev <artem@supernova.io>
2023-08-25 18:04:25 +0200

    Exclude isNonVirtualRoot group from name path

--- src/functions.js
+++ src/functions.js
@@ -1,49 +1,56 @@
 /**
- * 
- * @param {string} text 
- * @param {string} indentationString 
- * 
+ *
+ * @param {string} text
+ * @param {string} indentationString
+ *
  * @returns {string}
  */
 function createDocumentationComment(text, indentationString) {
-    return text.trim().split("\n").map((line) => `${indentationString}/// ${line}`).join("\n")
+  return text
+    .trim()
+    .split("\n")
+    .map((line) => `${indentationString}/// ${line}`)
+    .join("\n");
 }
 
 /**
- * 
- * @param {{name: string, isRoot: boolean, path: Array<string>}} tokenGroup 
- * 
+ *
+ * @param {{name: string, isRoot: boolean, path: Array<string>}} tokenGroup
+ *
  * @returns {Array<string>}
  */
 function createFullTokenGroupPath(tokenGroup) {
-    if (tokenGroup.isRoot) {
-        return []
-    } else {
-        return tokenGroup.path.concat(tokenGroup.name)
-    }
+  if (tokenGroup.isRoot || tokenGroup.isNonVirtualRoot) {
+    return [];
+  } else {
+    return tokenGroup.path.concat(tokenGroup.name);
+  }
 }
 
 /**
- * 
+ *
  * @param {Array<any>} lhs
  * @param {Array<any>} rhs
- * 
+ *
  * @returns {Array<any>}
  */
 function arrayConcat(lhs, rhs) {
-    return lhs.concat(rhs)
+  return lhs.concat(rhs);
 }
 
 /**
- * 
- * @param {Array<string>} array 
- * @param {string} separator 
+ *
+ * @param {Array<string>} array
+ * @param {string} separator
  */
 function arrayJoin(array, separator) {
-    return array.join(separator)
+  return array.join(separator);
 }
 
-Pulsar.registerFunction("createDocumentationComment", createDocumentationComment)
-Pulsar.registerFunction("createFullTokenGroupPath", createFullTokenGroupPath)
-Pulsar.registerFunction("arrayConcat", arrayConcat)
-Pulsar.registerFunction("arrayJoin", arrayJoin)
+Pulsar.registerFunction(
+  "createDocumentationComment",
+  createDocumentationComment
+);
+Pulsar.registerFunction("createFullTokenGroupPath", createFullTokenGroupPath);
+Pulsar.registerFunction("arrayConcat", arrayConcat);
+Pulsar.registerFunction("arrayJoin", arrayJoin);
